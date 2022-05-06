12733989e0896c070beae25ea0108d3f6107bd31
ben-gee723 <ben.m.goldschmidt@gmail.com>
2022-05-06 13:56:26 +0200

    chartjs removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import React, { useReducer } from 'react';
-import UserDashboard from './components/UserDashboard';
 import Counter from './components/Counter';
 
 
 // Import Styles
 import './styles/App.css';
-import { LineCharReact } from './components/lineChartReact';
-import LineChartReactMartin from './components/LineChartReactMartin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome!</h1>
-        {/* <Counter /> */}
-        <LineChartReactMartin />
-
+        <Counter />
       </header>
     </div>
   )
