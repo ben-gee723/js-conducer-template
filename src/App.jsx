5734a19583e14fb313c83b76d00e4fafb802cb39
ben-gee723 <ben.m.goldschmidt@gmail.com>
2022-05-06 13:50:28 +0200

    lineChart test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React, { useReducer } from 'react';
 import UserDashboard from './components/UserDashboard';
-import Counter from './components/Counter'
+import Counter from './components/Counter';
 
 
 // Import Styles
 import './styles/App.css';
+import { LineCharReact } from './components/lineChartReact';
+import LineChartReactMartin from './components/LineChartReactMartin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome!</h1>
-        <Counter />
+        {/* <Counter /> */}
+        <LineChartReactMartin />
+
       </header>
     </div>
   )
